f1bc2d6f00fd5f24775dcffedfd89b476ce1efdb
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-08-06 20:23:51 -0500

    added get and post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
 // *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
+const db = require("../models");
+const todo = require("../models/todo");
 
 // Dependencies
 // Routes
@@ -9,12 +11,17 @@ module.exports = function(app) {
 
   // GET route for getting all of the todos
   app.get("/api/todos", function(req, res) {
-    res.end()
+    db.Todo.findAll().then( (data) => {
+      res.json(data);
+    });
   });
 
   // POST route for saving a new todo. We can create a todo using the data on req.body
   app.post("/api/todos", function(req, res) {
-    res.end()
+    console.log(req.body);
+    db.Todo.create(req.body).then( (data) => {
+      res.json(data);
+    });
   });
 
   // DELETE route for deleting todos. We can access the ID of the todo to delete in
